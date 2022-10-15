8344353deb77b0a1de6a839164ed690fe281952c
Michael Hu <pxmyhdev@gmail.com>
2022-10-15 13:02:41 -0500

    Add information

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import logo from './assets/logo.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableOpacity
+        onPress={() => alert('You picked Austin!')}
+        style={{ backgroundColor: 'green' }}
+      >
+        <Text style={{ fontSize: 16, color: '#fff' }}>Pick a City</Text>
+      </TouchableOpacity>
+      <Image source={logo} style={{ width: 305, height: 159 }} />
+      <Text>Settle Down at Austin</Text>
+      <Text>Moving from Outside of United States</Text>
+      <Text>Auto Import</Text>
+      <Text>* Get CBP approved HS-7 form, make sure they stamp and sign</Text>
+      <Text>* Get VIN inspection [More on this]</Text>
+      <Text>
+        * Get State vehicle safety inspection [More on this, sponsorship link]
+      </Text>
+      <Text>
+        * Go to vehicle registration office, usually texas tax collection county
+      </Text>
+      <Text>* Wait for title to be mailed</Text>
+      <Text>fill</Text>
+      <Text>fill</Text>
+      <Text>fill</Text>
+      <Text>fill</Text>
+      <Text>fill</Text>
+      <Text>fill</Text>
+      <Text>Lease House</Text>
+      <StatusBar style='auto' />
     </View>
   );
 }
